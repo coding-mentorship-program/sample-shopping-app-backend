110ddea4beadc6139f0dbcdc45859ae1527050cc
jonathans199 <jso1919@gmail.com>
2022-08-26 21:14:55 -0400

    full crud BED working

--- index.js
+++ index.js
@@ -10,14 +10,31 @@ const products = database.collection('products')
 
 client.connect()
 console.log('connected to mongo')
+const PORT = process.env.PORT
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.listen(4040, () => console.log('API running on port 4040'))
+app.listen(PORT, () => console.log('API running on port 4040'))
 
 app.get('/', async (req, res) => {
   const allProducts = await products.find().toArray()
   res.send(allProducts)
+})
+
+app.post('/', async (req, res) => {
+  await products.insertOne(req.body)
+  res.json('Item was added')
+})
+
+app.delete('/', async (req, res) => {
+  await products.findOneAndDelete(req.query)
+  res.json('Item was deleted')
+})
+
+// PUT - like a delete, then like a POST
+app.put('/', async (req, res) => {
+  await products.findOneAndUpdate(req.query, { $set: req.body })
+  res.json('Item was updated with special field')
 })
