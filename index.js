d752cd35c4f3be7376533d3723c5339901ac945e
jonathans199 <jso1919@gmail.com>
2023-09-03 13:17:26 -0400

    updated with params

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import { MongoClient } from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 import 'dotenv/config'
 
 const URI = process.env.MONGO_URI
@@ -19,22 +19,28 @@ app.use(express.json())
 app.listen(PORT, () => console.log('API running on port 4040'))
 
 app.get('/', async (req, res) => {
-  const allProducts = await products.find().toArray()
-  res.send(allProducts)
+	const allProducts = await products.find().toArray()
+	res.send(allProducts)
+})
+
+app.get('/single-post/:id', async (req, res) => {
+	const id = new ObjectId(req.params.id)
+	const itemFound = await products.findOne({ _id: id })
+	res.send(itemFound)
 })
 
 app.post('/', async (req, res) => {
-  await products.insertOne(req.body)
-  res.json('Item was added')
+	await products.insertOne(req.body)
+	res.json('Item was added')
 })
 
 app.delete('/', async (req, res) => {
-  await products.findOneAndDelete(req.query)
-  res.json('Item was deleted')
+	await products.findOneAndDelete(req.query)
+	res.json('Item was deleted')
 })
 
 // PUT - like a delete, then like a POST
 app.put('/', async (req, res) => {
-  await products.findOneAndUpdate(req.query, { $set: req.body })
-  res.json('Item was updated with special field')
+	await products.findOneAndUpdate(req.query, { $set: req.body })
+	res.json('Item was updated with special field')
 })
