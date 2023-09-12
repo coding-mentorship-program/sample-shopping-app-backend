dcdfdb902779486815f49bc9d7010af6da031686
jonathans199 <jso1919@gmail.com>
2023-09-11 23:52:00 -0400

    updated user workflow with bcrypt

--- users.js
+++ users.js
@@ -1,25 +1,40 @@
+import bcrypt from 'bcrypt'
 import { users } from './index.js'
 
 export const addUser = async (req, res) => {
-	const userAdded = await users.insertOne(req.body)
+	// check if email has @ and a .com, .net
+	// check if password meets minimum requirements (8 or more, complex)
+	if (req.body.email && req.body.password) {
+		try {
+			const hashedPassword = await bcrypt.hash(req.body.password, 10)
 
-	res.send(userAdded)
+			const userAdded = await users.insertOne({ email: req.body.email.toLowerCase(), password: hashedPassword })
+			res.send(userAdded)
+		} catch (err) {
+			console.log(err)
+			res.send(err)
+		}
+	}
 }
 
 export const loginUser = async (req, res) => {
 	try {
 		const userFound = await users.findOne({ email: req.body.email })
-		const dbPassword = userFound.password // password saved in db
-		const passwordSent = req.body.password // password coming in from body
 
-		if (userFound && dbPassword === passwordSent) {
-			res.send(userFound)
+		if (userFound) {
+			bcrypt.compare(req.body.password, userFound.password, (err, passwordOk) => {
+				if (!err && passwordOk) {
+					res.send(userFound)
+				} else {
+					console.log('err -> ', err)
+					res.send({ error: 'User not found or password incorrect' })
+				}
+			})
 		} else {
-			res.send('User NOT Allowed')
+			res.send({ error: 'User not found or password incorrect' })
 		}
 	} catch (err) {
 		console.log(err)
+		res.send(err)
 	}
-
-	// we need to add user into mongodb
 }
